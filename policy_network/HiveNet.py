d39695dabb698a6dbc5f2276a001ffa95b84f88b
PatrykRadon <radon.patryk@gmail.com>
2020-05-03 17:45:35 +0200

    Splited to actor and critic

--- policy_network/HiveNet.py
+++ policy_network/HiveNet.py
@@ -9,15 +9,22 @@ class HiveNet(nn.Module):
     def __init__(self, frame_height, frame_width, kernel_size, stride, num_of_clusters):
         super(HiveNet, self).__init__()
         self.vision = HiveNetVision(frame_height, frame_width, kernel_size, stride, outputs=32)
-        self.hidden1 = nn.Linear(32+num_of_clusters, 64)
-        self.output = nn.Linear(64, 2*num_of_clusters)
+        self.policy_hidden1 = nn.Linear(32+num_of_clusters, 64)
+        self.policy_output = nn.Linear(64, 2*num_of_clusters)
+
+        self.value_hidden1 = nn.Linear(32 + num_of_clusters, 64)
+        self.value_output = nn.Linear(64, 2 * num_of_clusters)
 
     def forward(self, map_input, thresholds):
         x = self.vision(map_input)
         x = torch.cat((x, thresholds), dim=1)
-        x = F.relu(self.hidden1(x))
-        x = F.relu(self.output(x))
-        return x
+
+        actor_x = F.relu(self.policy_hidden1(x))
+        action_probabilities = F.relu(self.policy_output(actor_x))
+
+        critic_x = F.relu(self.value_hidden1(x))
+        values = F.relu(self.value_output(critic_x))
+        return action_probabilities, values
 
 
 
