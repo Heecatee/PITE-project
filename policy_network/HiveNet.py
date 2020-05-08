1768e7b2766464d567a2a6e4c593046d8568654b
PatrykRadon <radon.patryk@gmail.com>
2020-05-08 15:01:21 +0200

    Code readability improvements

--- policy_network/HiveNet.py
+++ policy_network/HiveNet.py
@@ -5,15 +5,23 @@ from hive_vision import HiveNetVision
 
 
 class HiveNet(nn.Module):
+    def __init__(self, kernel_size, stride, num_of_thresholds,
+                 hidden_layer_size=64,
+                 vision_net_output=32,
+                 actions_per_threshold=2):
 
-    def __init__(self, kernel_size, stride, num_of_clusters):
         super(HiveNet, self).__init__()
-        self.vision = HiveNetVision(kernel_size, stride, outputs=32)
-        self.policy_hidden1 = nn.Linear(32+num_of_clusters, 64)
-        self.policy_output = nn.Linear(64, 2*num_of_clusters)
+        self.vision = HiveNetVision(kernel_size, stride, outputs=vision_net_output)
+        self.policy_hidden1 = nn.Linear(in_features=vision_net_output + num_of_thresholds,
+                                        out_features=hidden_layer_size)
+        possible_actions_size = actions_per_threshold*num_of_thresholds
+        self.policy_output = nn.Linear(in_features=hidden_layer_size,
+                                       out_features=possible_actions_size)
 
-        self.value_hidden1 = nn.Linear(32 + num_of_clusters, 64)
-        self.value_output = nn.Linear(64, 2 * num_of_clusters)
+        self.value_hidden1 = nn.Linear(in_features=vision_net_output + num_of_thresholds,
+                                       out_features=hidden_layer_size)
+        self.value_output = nn.Linear(in_features=hidden_layer_size,
+                                      out_features=possible_actions_size)
 
     def forward(self, map_input, thresholds):
         x = self.vision(map_input)
--- policy_network/hive_vision/HiveNetVision.py
+++ policy_network/hive_vision/HiveNetVision.py
@@ -7,22 +7,28 @@ from PIL import Image
 
 class HiveNetVision(nn.Module):
 
-    def __init__(self, kernel_size, stride, outputs, starting_frame):
+    def __init__(self, kernel_size, stride, outputs, starting_frame,
+                 hidden_layer_dims=(16, 32),
+                 frames_per_input=3,
+                 image_compressed_size=60):
         super(HiveNetVision, self).__init__()
         self.process_image_input = T.Compose([T.Grayscale(),
-                                              T.Resize(60, interpolation=Image.CUBIC)])
+                                              T.Resize(image_compressed_size, interpolation=Image.CUBIC)])
         self.map_history = [starting_frame, starting_frame, starting_frame]
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=kernel_size, stride=stride)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=kernel_size, stride=stride)
-        self.bn2 = nn.BatchNorm2d(32)
+        hidden_layer1_size, hidden_layer2_size = hidden_layer_dims
+        self.conv1 = nn.Conv2d(in_channels=frames_per_input, out_channels=hidden_layer1_size,
+                               kernel_size=kernel_size, stride=stride)
+        self.bn1 = nn.BatchNorm2d(hidden_layer1_size)
+        self.conv2 = nn.Conv2d(in_channels=hidden_layer1_size, out_channels=hidden_layer2_size,
+                               kernel_size=kernel_size, stride=stride)
+        self.bn2 = nn.BatchNorm2d(hidden_layer2_size)
 
-        def conv2d_size_out(size, kernel_size=kernel_size, stride=stride):
+        def conv2d_size(size, kernel_size=kernel_size, stride=stride):
             return (size - kernel_size) // stride + 1
 
-        convw = conv2d_size_out(conv2d_size_out(60))
-        convh = conv2d_size_out(conv2d_size_out(60))
-        linear_input_size = convw * convh * 32
+        conv_width = conv2d_size(conv2d_size(image_compressed_size))
+        conv_height = conv2d_size(conv2d_size(image_compressed_size))
+        linear_input_size = conv_width * conv_height * hidden_layer2_size
         self.output = nn.Linear(linear_input_size, outputs)
 
     def forward(self, map_image):
@@ -31,7 +37,7 @@ class HiveNetVision(nn.Module):
         x = torch.Tensor(self.map_history)
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
-        x_flatten = torch.flatten(x)
-        x = self.output(x_flatten)
+        x = torch.flatten(x)
+        x = self.output(x)
         return x
 
