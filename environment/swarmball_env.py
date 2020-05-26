4d62fdf088505a2ccd6854d74a40236ee2ee84a7
ichtiostega <ichtiostega@hal-9000.kocierz.com>
2020-05-26 23:22:45 +0200

    Did some cleanup

--- environment/swarmball_env.py
+++ environment/swarmball_env.py
@@ -4,17 +4,11 @@ from gym.utils import seeding
 from .simulation.simulation import SwarmBallSimulation
 
 class SwarmBall(gym.Env):
-    def __init__(self, acc_factor=0.25, thresh_amount=3):
-        self.sim = SwarmBallSimulation()
+    def __init__(self, acc_factor=0.25, thresh_amount=3, **kwargs):
+        self.sim = SwarmBallSimulation(**kwargs)
         self.thresholds = [[] for _ in range(thresh_amount)]
         self.thresh_vel = [0,0,0]
-        self.acc_factor = 0.25
-
-    def seed(self, seed=None):
-        """
-            To przerażające nasiono!!!
-        """
-        pass
+        self.acc_factor = acc_factor
 
     def reward(self):
         pass
@@ -22,9 +16,7 @@ class SwarmBall(gym.Env):
     def step(self, action):
         self.thresholds.pop(0)
         self.thresholds.append(self.sim.threshold_positions())
-
         self.thresh_vel = [self.thresh_vel[i] + action[i]*self.acc_factor for i in range(len(self.thresh_vel))]
-
         for i in range(len(action)):
             self.sim.update_thresholds_position(i, self.sim.threshold_positions()[i] + self.thresh_vel[i])
         self.sim.step()
