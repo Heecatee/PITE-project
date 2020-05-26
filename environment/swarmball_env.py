5e8104ade2c9acf7ed102ceeffeeb60a481d85a8
ichtiostega <ichtiostega@hal-9000.kocierz.com>
2020-05-26 21:51:22 +0200

    WIP

--- environment/swarmball_env.py
+++ environment/swarmball_env.py
@@ -6,6 +6,7 @@ from .simulation.simulation import SwarmBallSimulation
 class SwarmBall(gym.Env):
     def __init__(self):
         self.sim = SwarmBallSimulation()
+        self.thresholds = [[],[],[]]
 
     def seed(self, seed=None):
         """
@@ -13,11 +14,22 @@ class SwarmBall(gym.Env):
         """
         pass
 
+    def reward(self):
+        pass
+
     def step(self, action):
+        self.thresholds.pop(0)
+        self.thresholds.append(self.sim.threshold_positions())
+        for i in range(len(action)):
+            print(i, action[i])
+            self.sim.update_thresholds_position(i, action[i])
         self.sim.step()
+        observations = {'picture': self.sim.space_near_goal_object, 'past_thresholds': self.thresholds}
+        return observations, self.reward(), False, {}
 
     def reset(self):
         self.sim.reset()
+        self.goal_prev_pos = self.sim._goal_object.body.position[0]
 
     def render(self):
         self.sim.redraw()
