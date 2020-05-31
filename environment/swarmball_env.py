808b8812d2d5429d0f5973507b5c46339ef3c8f6
Wojciech Korzybski <wojciech.korzybski@zhp.net.pl>
2020-05-31 14:37:59 +0200

    Minor changes in data collector, removed net from a2c, created packages, prepered everything to merge in main file

--- environment/swarmball_env.py
+++ environment/swarmball_env.py
@@ -7,6 +7,7 @@ try:
 except ImportError:
     from simulation.simulation import SwarmBallSimulation
 
+
 class SwarmBall(gym.Env):
     def __init__(self, acc_factor=0.25, number_of_clusters=3, **kwargs):
         self.sim = SwarmBallSimulation(number_of_clusters, **kwargs)
@@ -18,12 +19,15 @@ class SwarmBall(gym.Env):
         pass
 
     def step(self, action):
-        self.thresh_vel = [self.thresh_vel[i] + action[i]*self.acc_factor for i in range(self.cluster_count)]
+        self.thresh_vel = [self.thresh_vel[i] + action[i] *
+                           self.acc_factor for i in range(self.cluster_count)]
         for i in range(self.cluster_count):
-            self.sim.update_thresholds_position(i, self.sim.threshold_positions()[i] + self.thresh_vel[i])
+            self.sim.update_thresholds_position(
+                i, self.sim.threshold_positions()[i] + self.thresh_vel[i])
         self.sim.step()
-        observations = {'picture': self.sim.space_near_goal_object(0), 'thresholds': self.sim.threshold_positions()}
-        return observations, self.reward(), self.sim._enemy_position >= self.sim._goal_object.body.position[0] , {'message': 'You look great today cutiepie!'}
+        observations = {'picture': self.sim.space_near_goal_object(
+            0), 'thresholds': self.sim.threshold_positions()}
+        return observations, self.reward(), self.sim._enemy_position >= self.sim._goal_object.body.position[0], {'message': 'You look great today cutiepie!'}
 
     def reset(self):
         self.thresh_vel = [0 for _ in range(self.cluster_count)]
