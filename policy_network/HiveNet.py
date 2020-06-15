75673165534b286dcb9f31a55c38df383cc1f7e1
PatrykRadon <radon.patryk@gmail.com>
2020-06-15 16:15:44 +0200

    SB-63 Change NN to use last 2 threshold positions

--- policy_network/HiveNet.py
+++ policy_network/HiveNet.py
@@ -5,55 +5,73 @@ from torch.nn import functional as F
 from torch.distributions import Categorical
 from .hive_vision.HiveNetVision import HiveNetVision
 
+
 class HiveNet(nn.Module):
-  def __init__(self, kernel_size, stride, num_of_thresholds,
-               hidden_layer_size=64,
-               vision_net_output=32,
-               actions_per_threshold=2):
+    def __init__(self, kernel_size, stride, num_of_thresholds,
+                 hidden_layer_size=64,
+                 vision_net_output=32,
+                 actions_per_threshold=2,
+                 time_steps_stored=2):
 
-    super(HiveNet, self).__init__()
-    self.vision = HiveNetVision(kernel_size, stride, outputs=vision_net_output)
-    self.policy_hidden1 = nn.Linear(in_features=vision_net_output + num_of_thresholds,
-                                    out_features=hidden_layer_size)
-    self.num_of_thresholds = num_of_thresholds
-    possible_actions_size = actions_per_threshold * self.num_of_thresholds
-    self.policy_output = nn.Linear(in_features=hidden_layer_size,
-                                   out_features=possible_actions_size)
+        super(HiveNet, self).__init__()
+        self.vision = HiveNetVision(kernel_size, stride, outputs=vision_net_output)
+        self.policy_hidden1 = nn.Linear(in_features=vision_net_output + 2 * self.num_of_thresholds,
+                                        out_features=hidden_layer_size)
+        self.num_of_thresholds = num_of_thresholds
+        self.thresholds_history = None
+        self.time_steps_stored = time_steps_stored
+        possible_actions_size = actions_per_threshold * self.num_of_thresholds
+        self.policy_output = nn.Linear(in_features=hidden_layer_size,
+                                       out_features=possible_actions_size)
 
-    self.value_hidden1 = nn.Linear(in_features=vision_net_output + self.num_of_thresholds,
-                                   out_features=hidden_layer_size)
-    self.value_output = nn.Linear(in_features=hidden_layer_size,
-                                  out_features=1)
+        self.value_hidden1 = nn.Linear(in_features=vision_net_output + 2 * self.num_of_thresholds,
+                                       out_features=hidden_layer_size)
+        self.value_output = nn.Linear(in_features=hidden_layer_size,
+                                      out_features=1)
 
-  def pick_action(self, map_input, thresholds, collector):
-    x = self.vision(map_input)
-    x = torch.cat((x, torch.Tensor(thresholds)))
+    def update_thresholds_history(self, new_thresholds):
+        if self.thresholds_history is None:
+            self.thresholds_history = []
+            for _ in range(self.time_steps_stored):
+                self.thresholds_history.append(new_thresholds)
+        else:
+            self.thresholds_history.pop(0)
+            self.thresholds_history.append(new_thresholds)
+        return self.thresholds_history
 
-    actor_x = F.relu(self.policy_hidden1(x))
-    actor_x = F.relu(self.policy_output(actor_x))
-    action_probabilities = F.softmax(actor_x)
-    distribution = Categorical(action_probabilities)
-    action = distribution.sample()
+    def pick_action(self, map_input, thresholds, collector):
+        new_thresholds = torch.Tensor(thresholds)
+        self.update_thresholds_history(new_thresholds)
 
-    collector.states.append(x)
-    collector.actions.append(action)
-    collector.action_logarithms.append(
-        distribution.log_prob(action))
-    def bit_representation(action, num_bits):
-        return np.unpackbits(np.uint8(action))[-num_bits:]
-        
-    return bit_representation(action.item(), num_bits = self.num_of_thresholds)
+        x = self.vision(map_input)
+        x = torch.cat([x, *self.thresholds_history])
 
-  def evaluate(self, state, action):
-    actor_x = F.relu(self.policy_hidden1(state))
-    actor_x = F.relu(self.policy_output(actor_x))
-    probabilities = F.softmax(actor_x)
-    distribution = Categorical(probabilities)
-    logarithm_probabilities = distribution.log_prob(action)
-    entropy = distribution.entropy()
+        actor_x = F.relu(self.policy_hidden1(x))
+        actor_x = F.relu(self.policy_output(actor_x))
+        action_probabilities = F.softmax(actor_x)
+        distribution = Categorical(action_probabilities)
+        action = distribution.sample()
 
-    critic_x = F.relu(self.value_hidden1(state))
-    critic_x = F.relu(self.value_output(critic_x))
-    Qvalue = F.tanh(critic_x)
+        collector.states.append(x)
+        collector.actions.append(action)
+        collector.action_logarithms.append(
+            distribution.log_prob(action))
 
-    return logarithm_probabilities, torch.squeeze(Qvalue), entropy
+        def bit_representation(action, num_bits):
+            return np.unpackbits(np.uint8(action))[-num_bits:]
+
+        return bit_representation(action.item(), num_bits=self.num_of_thresholds)
+
+    def evaluate(self, state, action):
+        actor_x = F.relu(self.policy_hidden1(state))
+        actor_x = F.relu(self.policy_output(actor_x))
+        probabilities = F.softmax(actor_x)
+        distribution = Categorical(probabilities)
+        logarithm_probabilities = distribution.log_prob(action)
+        entropy = distribution.entropy()
+
+        critic_x = F.relu(self.value_hidden1(state))
+        critic_x = F.relu(self.value_output(critic_x))
+        Qvalue = F.tanh(critic_x)
+
+        return logarithm_probabilities, torch.squeeze(Qvalue), entropy
